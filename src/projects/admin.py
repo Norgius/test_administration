5a9e22b90935d47161b620c54c3d8d7d6be1074e
Norgius <dmitry.schadrin21@yandex.ru>
2024-01-10 17:19:27 +0300

    Admin settings

--- src/projects/admin.py
+++ src/projects/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project, Task, Employee
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+class TaskInline(admin.TabularInline):
+    model = Employee.tasks.through
+    raw_id_fields = ('task',)
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'position')
+    raw_id_fields = ('tasks',)
+    inlines = [TaskInline]
