f1bb43d524be24ee272379d2066f359509f16c6b
Norgius <dmitry.schadrin21@yandex.ru>
2024-01-11 23:48:45 +0300

    Update admin

--- src/projects/admin.py
+++ src/projects/admin.py
@@ -1,16 +1,29 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
 
-from .models import Project, Task, Employee
+from .models import Project, Task, Employee, Comment
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'is_active']
+    list_filter = ['is_active']
+    search_fields = ['name']
 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'deadline', 'status']
+    list_display_links = ['name', 'deadline']
+    list_filter = ['status']
+    search_fields = ['name', 'project__name']
+    readonly_fields = ['deadline']
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        queryset = super().get_queryset(request)
+        return queryset.select_related('project')
 
 
 class TaskInline(admin.TabularInline):
@@ -20,6 +33,12 @@ class TaskInline(admin.TabularInline):
 
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'position')
-    raw_id_fields = ('tasks',)
+    list_display = ['name', 'position']
+    fields = ['name', 'position']
     inlines = [TaskInline]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['written_in', 'task']
+    readonly_fields = ['written_in']
