fb836681d16515350b59f7e1a7e408b87f2bcffb
Norgius <dmitry.schadrin21@yandex.ru>
2024-01-11 17:30:29 +0300

    Add command to populate db with testdata

---
+++ src/projects/management/commands/populate_db.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+from random import choice, choices
+
+from django.core.management.base import BaseCommand
+from projects.models import Project, Task, Employee
+
+
+class Command(BaseCommand):
+    help = 'Populate DB with data'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--multiplier', type=int, default=1, help='Data multiplier')
+        parser.add_argument('--fio', type=str, default='fio.txt', help='File path')
+        parser.add_argument('--positions', type=str, default='positions.txt', help='File path')
+        parser.add_argument('--sentences', type=str, default='sentences.txt', help='File path')
+        parser.add_argument('--words', type=str, default='words.txt', help='File path')
+        parser.add_argument('--deadline', type=str, help='Deadline for task "day-month-year"-"30-01-2024"')
+
+    def handle(self, *args, **kwargs):
+
+        # Project.objects.all().delete()
+        # Task.objects.all().delete()
+        # Employee.objects.all().delete()
+        fio = kwargs.get('fio')
+        positions = kwargs.get('positions')
+        sentences = kwargs.get('sentences')
+        words = kwargs.get('words')
+        multiplier = kwargs.get('multiplier')
+        try:
+            with open(words, 'r') as w, open(sentences, 'r') as s:
+                sentences = s.readlines()
+                words = w.readlines()
+            with open(positions, 'r') as p, open(fio, 'r') as f:
+                positions = p.readlines()
+                fio = f.readlines()
+        except FileNotFoundError as e:
+            print(f'{e.strerror}: {e.filename}')
+            exit()
+        for _ in range(multiplier):
+            projects = [
+                Project(
+                    name=choice(words),
+                    description=choice(sentences)
+                )
+                for _ in range(25)
+            ]
+            date = datetime.strptime(kwargs.get('deadline'), '%d-%m-%Y').date()
+            projects = Project.objects.bulk_create(projects)
+            tasks = [
+                Task(
+                    name=choice(words),
+                    description=choice(sentences),
+                    deadline=date,
+                    project=choice(projects),
+                )
+                for _ in range(300)
+            ]
+            tasks = Task.objects.bulk_create(tasks)
+
+            employees = [
+                Employee(
+                    name=choice(fio),
+                    position=choice(positions),
+                )
+                for _ in range(25)
+            ]
+            employees = Employee.objects.bulk_create(employees)
+            for employee in employees:
+                employee.tasks.set(choices(tasks, k=25))
+
+        print('Данные успешно добавлены')
