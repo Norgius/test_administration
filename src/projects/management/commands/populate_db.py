bb72a4b143dd8a213c01b90731d35af797e78235
Norgius <dmitry.schadrin21@yandex.ru>
2024-01-12 10:18:08 +0300

    Extend populate_db command functional

--- src/projects/management/commands/populate_db.py
+++ src/projects/management/commands/populate_db.py
@@ -1,11 +1,33 @@
 from datetime import datetime
 from random import choice, choices
+from itertools import chain
 
 from django.core.management.base import BaseCommand
 from projects.models import Project, Task, Employee
 from django.conf import settings
 
 base_dir = settings.BASE_DIR
+
+
+def create_tasks(
+        words: list[str],
+        sentences: list[str],
+        date: datetime,
+        projects: list[Project],
+        task_status: Task.TaskStatus,
+) -> list[Task]:
+    tasks = [
+        Task(
+            name=choice(words),
+            description=choice(sentences),
+            deadline=date,
+            project=choice(projects),
+            status=task_status,
+        )
+        for _ in range(150)
+    ]
+    tasks = Task.objects.bulk_create(tasks)
+    return tasks
 
 
 class Command(BaseCommand):
@@ -17,18 +39,19 @@ class Command(BaseCommand):
         parser.add_argument('--positions', type=str, default=base_dir / 'testdata' / 'positions.txt', help='File path')
         parser.add_argument('--sentences', type=str, default=base_dir / 'testdata' / 'sentences.txt', help='File path')
         parser.add_argument('--words', type=str, default=base_dir / 'testdata' / 'words.txt', help='File path')
-        parser.add_argument('--deadline', type=str, help='Deadline for task "day-month-year"-"30-01-2024"')
+        parser.add_argument('--deadline', type=str, default='31-12-2024',
+                            help='Deadline for task "day-month-year"-"30-01-2024"'
+                            )
 
     def handle(self, *args, **kwargs):
 
-        # Project.objects.all().delete()
-        # Task.objects.all().delete()
-        # Employee.objects.all().delete()
         fio = kwargs.get('fio')
         positions = kwargs.get('positions')
         sentences = kwargs.get('sentences')
         words = kwargs.get('words')
         multiplier = kwargs.get('multiplier')
+        date = datetime.strptime(kwargs.get('deadline'), '%d-%m-%Y').date()
+
         try:
             with open(words, 'r') as w, open(sentences, 'r') as s:
                 sentences = s.readlines()
@@ -39,36 +62,44 @@ class Command(BaseCommand):
         except FileNotFoundError as e:
             print(f'{e.strerror}: {e.filename}')
             exit()
+
         for _ in range(multiplier):
-            projects = [
+
+            active_projects = [
                 Project(
                     name=choice(words),
-                    description=choice(sentences)
+                    description=choice(sentences),
                 )
                 for _ in range(25)
             ]
-            date = datetime.strptime(kwargs.get('deadline'), '%d-%m-%Y').date()
-            projects = Project.objects.bulk_create(projects)
-            tasks = [
-                Task(
+
+            not_active_projects = [
+                Project(
                     name=choice(words),
                     description=choice(sentences),
-                    deadline=date,
-                    project=choice(projects),
+                    is_active=False,
                 )
-                for _ in range(300)
+                for _ in range(15)
             ]
-            tasks = Task.objects.bulk_create(tasks)
+
+            active_projects = Project.objects.bulk_create(active_projects)
+            not_active_projects = Project.objects.bulk_create(not_active_projects)
+
+            _ = create_tasks(words, sentences, date, active_projects, Task.TaskStatus.NEW)
+            tasks_in_process = create_tasks(words, sentences, date, active_projects, Task.TaskStatus.IN_PROCESS)
+            closed_tasks = create_tasks(words, sentences, date, not_active_projects, Task.TaskStatus.CLOSED)
+
+            task_for_employees = list(chain(tasks_in_process, closed_tasks))
 
             employees = [
                 Employee(
                     name=choice(fio),
                     position=choice(positions),
                 )
-                for _ in range(25)
+                for _ in range(30)
             ]
             employees = Employee.objects.bulk_create(employees)
             for employee in employees:
-                employee.tasks.set(choices(tasks, k=25))
+                employee.tasks.set(choices(task_for_employees, k=30))
 
         print('Данные успешно добавлены')
